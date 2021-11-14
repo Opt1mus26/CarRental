dfc7cb232a9fe2b2bb59f0f2c8a82775cab19ec0
Opt1mus26 <pab1o@mail.ru>
2021-11-14 21:14:36 +0300

    Initial Commit

---
+++ CarRental/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarRental
+//
+//  Created by Павел Цыганов on 14.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
