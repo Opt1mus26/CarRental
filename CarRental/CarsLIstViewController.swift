14a3e96a6e70ec2b85a2b3c217725e643d1d5557
Alisa Ts <freiheiit92@mail.ru>
2021-11-22 15:15:56 +0300

    delete VC's

--- CarRental/CarsLIstViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CarsLIstViewController.swift
-//  CarRental
-//
-//  Created by Alisa Ts on 15.11.2021.
-//
-
-import UIKit
-
--- CarRental/FirstViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  FirstViewController.swift
-//  CarRental
-//
-//  Created by Alisa Ts on 18.11.2021.
-//
-
-import UIKit
-
